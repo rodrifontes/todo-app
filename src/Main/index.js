3ef7cd1d33254c20c45cc2580b968db40f02c6b4
Rodrigo Fontes <rodrigo.cruz@ifs.edu.br>
2024-05-24 21:00:04 -0300

    First Commit

---
+++ src/Main/index.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+
+import { Text } from '../components/Text';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
