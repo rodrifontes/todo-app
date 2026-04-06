bfd42b7eeaa1859ebcdb17cebd291e13c5cd02a1
Rodrigo Fontes <rodrifontes@gmail.com>
2026-04-06 14:50:06 -0300

    feat: Font Personalizada, SafeAreaView, Component Header

---
+++ src/Main/index.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
