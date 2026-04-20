272529601b4c9cf5f803c67fb155d4eff92e25a8
Rodrigo Fontes <rodrifontes@gmail.com>
2026-04-20 20:57:46 -0300

    feat: Add component Tasks

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,11 +1,33 @@
+import { tasks } from '../mocks/tasks';
+
 import Header from '../components/Header';
+import Tasks from '../components/Tasks';
 
 import { Container } from './styles';
 
 export default function Main() {
+  function handleChangeStatus(id) {
+    alert(`Alterar Status da Tarefa ${id}`);
+  }
+
+  function handleEditTask(task) {
+    alert(`Alterar Tarefa {${task.id}, ${task.title}, ${task.description}`);
+  }
+
+  function handleDeleteTask(id) {
+    alert(`Deletar Tarefa ${id}`);
+  }
+
   return (
     <Container>
       <Header />
+
+      <Tasks
+        tasks={tasks}
+        onChangeStatus={handleChangeStatus}
+        onEditTask={handleEditTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </Container>
   );
 }
